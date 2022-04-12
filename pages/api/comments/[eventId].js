7cbd4d481d18e553879631b25c4563fbba1939f9
Pranto Mollick <prantocse01@gmail.com>
2022-04-12 17:36:09 +0600

    adding comments api routes

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -4,9 +4,27 @@ function handler(req, res) {
     if (req.method === 'POST') {
         const { email, name, text } = req.body;
 
+        if (
+          !email.includes("@") ||
+          !name ||
+          name.trim() === "" ||
+          !text ||
+          text.trim() === ""
+        ) {
+            res.status(422).json({message: 'invalid input!'});
+            return;
+        }
 
-
-
+        
+        const newComment = {
+            id: new Date().toISOString(), 
+            email, 
+            name, 
+            text
+        }
+        
+        console.log(newComment);
+        res.status(201).json({ message: 'Added Comment!', comment: newComment});
     }
 
     if (req.method === 'GET') {
