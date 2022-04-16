f55c90c28e5400b4d2de03ae576902978920d0b1
Pranto Mollick <prantocse01@gmail.com>
2022-04-17 00:57:04 +0600

    adding error Handling

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,16 @@
 import { MongoClient } from 'mongodb';
 
+async function connectDatabase() {
+  const client = await MongoClient.connect('mongodb://localhost:27017');
+  return client;
+}
+
+async function inserDocument(client, document) {
+  const db = client.db('events')
+  const collection = db.collection('newsletter');
+  await collection.insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -9,11 +20,22 @@ async function handler(req, res) {
       return; 
     }
 
-    const client = await MongoClient.connect('mongodb://localhost:27017/')
-    const db = client.db('events')
-    const collection = db.collection('newsletter');
-    await collection.insertOne({ email: userEmail });
-    client.close();
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({message: 'Connecting to the database failed!'});
+      return;
+    }
+    try {
+      await inserDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({message: 'Inserting Data failed!'});
+    }
+
+
+    
     console.log(userEmail);
     res.status(201).json({ message: "Signed up!" });
   }
