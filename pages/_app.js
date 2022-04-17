1b34b4368efcfb3ba0b5ccab4393de4efdf193dc
Pranto Mollick <prantocse01@gmail.com>
2022-04-17 16:09:22 +0600

    using context data in Component

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 
 import { NotificationContextProvider } from '../store/notification-context';
 import Layout from '../components/layout/layout';
-import Notification from '../components/ui/notification';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
@@ -15,7 +14,6 @@ function MyApp({ Component, pageProps }) {
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Component {...pageProps} />
-      <Notification title="test" message="this is test" status='success' />
     </Layout>
     </NotificationContextProvider>
   );
